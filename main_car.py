20bf4237526eab3d64b34219bf880d1587e804e9
Marcel Kowalczyk <44699834+Pices-26@users.noreply.github.com>
2021-02-06 08:08:16 -0500

    major changes
    Added a string checker for duplicate frame names + fix to this issue.
    Added gears for user to change on the go (fixed battery depletion issue)
    Added external drive for frames and CSV file (more efficient + easier to bring to main PC)

--- main_car.py
+++ main_car.py
@@ -11,7 +11,9 @@ import string
 from random import choice
 import cv2
 import pygame
+import time
 
+#we will need a car class
 class Car():
     def __init__(self):
         #servo init
@@ -20,7 +22,9 @@ class Car():
         self.turn = self.kit.servo[0]
         self.turn.set_pulse_width_range(1000,2000)
         self.speed = self.kit.continuous_servo[1]
-        self.frameDirectory = 'frames/'
+        self.frameDirectory =  '/media/marcel/New Volume/fyp/frames/'
+        self.csvDirectory =  '/media/marcel/New Volume/fyp/'
+        self.gear = 0.35
 
         #for saving data
         #0.check if document exists
@@ -30,8 +34,8 @@ class Car():
         #2.camera initialization
 
     def _csvCheck(self):
-        if path.exists('labeled_data.csv') == False:
-            with open('labeled_data.csv', 'wb') as csvfile:
+        if path.exists('{}labeled_data.csv'.format(self.csvDirectory)) == False:
+            with open('{}labeled_data.csv'.format(self.csvDirectory), 'wb') as csvfile:
                 pass
 
 
@@ -42,7 +46,7 @@ class Car():
 
     #maps range of turning to controller input range
     def _turnMapping(self, con_input):
-        turn_angle = np.interp(con_input,[-1,1],[175,45])
+        turn_angle = np.interp(con_input,[-1,1],[178,42])
         return turn_angle
 
     #we will need to reduce speed as it's too fast + invert steering
@@ -50,29 +54,53 @@ class Car():
         #inversing
         speed = con_input * -1.0
     
+        #reducing deadzone
         if speed > -0.0:
             #print("forward")
-            speed_new = np.interp(speed,[0.0,1.0],[0.8,1.0])
-            #reducing speed if needed
-            final_speed = speed_new * 1.0
+            speed_new = np.interp(speed,[0.0,1.0],[0.70,0.72])
+            #reducing speed
+            final_speed = speed_new * self.gear
             return final_speed
         elif speed < -0.0:
             #print("back")
-            speed_new = np.interp(speed,[-0.0,-1.0],[-0.8,-1.0])
+            speed_new = np.interp(speed,[-0.0,-1.0],[-0.8,-0.85])
             #reducing speed
-            final_speed = speed_new * 1.0
+            final_speed = speed_new * self.gear
             return final_speed
         else:
-            return speed  
+            return speed
+
+    def adjust_gears(self, gear_adjustement):
+        if self.gear >= 0.2 and self.gear <= 1.0:
+            self.gear = self.gear + gear_adjustement
+            print("new gear ={}".format(self.gear))
+
+        if self.gear < 0.20:
+            self.gear = 0.25
+        if self.gear > 1.0:
+            self.gear = 1.0
+        return
+
 
     def _saveTrainingData(self, axis, frame):
         numbers = '123456789'
         letters = string.ascii_lowercase
         nameOfFrame = ''.join(choice(letters+numbers) for i in range(25))
+        #checking if image with same name exists + fix if it does
+        nameOfFrame = self._checkName(nameOfFrame)
         cv2.imwrite(self.frameDirectory + nameOfFrame + '.jpg',frame)
-        with open('labeled_data.csv', 'a',newline='') as csvfile:
+        with open('{}labeled_data.csv'.format(self.csvDirectory), 'a',newline='') as csvfile:
             w = csv.writer(csvfile)
             w.writerow([nameOfFrame,axis[1],axis[2]])
+
+    def _checkName(self, name):
+        if path.isfile('{}{}{}'.format(self.frameDirectory,name, '.jpg')):
+            return(name)
+        else:
+            letter = choice(string.ascii_lowercase)
+            new_name = name + letter
+            return new_name
+
 
 
 
@@ -105,8 +133,20 @@ if __name__ == '__main__':
                     frame = cam._getFrame()
                     rc_car._saveTrainingData(axis, frame)
                 if buttons[3]:
-                    cam._stopThread()
+                    #cam._stopThread()
                     break
+                
+                #gear down
+                if buttons[4]:
+                    #print(buttons[4])
+                    rc_car.adjust_gears(-0.0005)
+                    #time.sleep(4)
+
+                #gear up
+                if buttons[5]:
+                    rc_car.adjust_gears(0.0005)
+                    #print(buttons[5])
+                    #time.sleep(4)
 
         #x = manual drive
         if buttons[1]:
@@ -114,7 +154,22 @@ if __name__ == '__main__':
             while True:
                 buttons, axis, hat = ps4_controller.listen()
                 rc_car._manualDrive(axis)
-                print("axis: ",axis[1])
+                #print("axis: ",axis[1])
+
+                #gear down
+                if buttons[4]:
+                    #print(buttons[4])
+                    rc_car.adjust_gears(-0.0005)
+                    #time.sleep(4)
+
+                #gear up
+                if buttons[5]:
+                    rc_car.adjust_gears(0.0005)
+                    #print(buttons[5])
+                    #time.sleep(4)
+                    
+                    
+
                 if buttons[3]:
                     break
         
